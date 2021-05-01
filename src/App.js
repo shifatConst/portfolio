07ad6fe6084f5764c03c3ed92a9eec25dd0efeb5
shifatreza <shifatreza44@gmail.com>
2021-05-01 23:22:29 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js'
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles 
+      params={{
+        particles: {
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              value_area: 900
+            }            
+          },
+          shape: {
+            type: "square",
+            stroke: {
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }}
+      />
+      <Navbar></Navbar>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css'
+import Typed from 'react-typed';
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1> <strong>Gazi Shifat Reza</strong> </h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Learner", "Web Developer", "Programmer"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="https://drive.google.com/file/d/1Kct2C2IHMVhaPn_ap3UYy27HPKMT-Lf2/view" target="_blank" className="btn-main-offer">Get My Resume</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
